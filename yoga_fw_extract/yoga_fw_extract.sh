8499bf5ecf81ef01570e404499d9cce2012ead93
charles <celliwig@nym.hush.com>
2019-11-20 13:37:06 +0000

    yoga_fw_extract: Create ath10k firmware files. Pretty print output.

--- yoga_fw_extract/yoga_fw_extract.sh
+++ yoga_fw_extract/yoga_fw_extract.sh
@@ -2,15 +2,22 @@
 
 # This script tries to extract the firmware files needed to enable the DSPs/audio/wifi on the Lenovo Yoga
 # from the systems windows partion
+###################################################################################################################################################
+
+URL_FW_FIRMWARE5BIN="https://github.com/kvalo/ath10k-firmware/raw/master/WCN3990/hw1.0/HL2.0/WLAN.HL.2.0-01387-QCAHLSWMTPLZ-1/firmware-5.bin"
+
+TXT_UNDERLINE="\033[1m\033[4m"
+TXT_NORMAL="\033[0m"
 
 # Don't run as root to avoid doing anything monumentally stupid
 if [ ${UID} -eq 0 ]; then
-	echo "Error: This script won't run as root."
+	echo -e "${TXT_UNDERLINE}Error: This script won't run as root.${TXT_NORMAL}"
 	exit
 fi
 
 WIN_PART=""									# Windows partition path
 # Try to automatically identify windows partition
+echo -e "${TXT_UNDERLINE}Getting Windows drivers...${TXT_NORMAL}"
 echo "Searching for Windows partition..."
 WIN_PART_LABEL="Windows4"
 WIN_PART_TMP=`blkid -L "${WIN_PART_LABEL}"`
@@ -134,6 +141,7 @@ fi
 
 # Process copied files
 if [ ${COPY_ERR} -eq 0 ]; then
+	echo -e "\n${TXT_UNDERLINE}Processing found drivers.${TXT_NORMAL}"
 	echo -n "Scanning copied files: "
 	# Get path to latest DSP files
 	DSP_FILE_CUR=`find Windows\ Drivers/ -type f -name qcadsp850.mbn -exec ls -t {} +|head -n1`
@@ -154,9 +162,10 @@ if [ ${COPY_ERR} -eq 0 ]; then
 	echo "Done"
 
 	# Create merged board file
-	echo "Creating merged board file..."
+	echo -e "\n${TXT_UNDERLINE}Merged board file:${TXT_NORMAL}"
 	PATH_BRD_MAKE="${CWD}/creating-board-2.bin"
 	PATH_BRD_SRC="${CWD}/creating-board-2.bin/bdf"
+	PATH_BRD_MFILE="board-2.bin"
 	if [ -e "${PATH_BRD_MAKE}" ]; then
 		echo "Deleting existing board file directory..."
 		rm -rf "${PATH_BRD_MAKE}"
@@ -216,7 +225,7 @@ if [ ${COPY_ERR} -eq 0 ]; then
 		fi
 
 		echo -n "Creating merged board file: "
-		python2 qca-swiss-army-knife/tools/scripts/ath10k/ath10k-bdencoder -c "${JSON}" -o board-2.bin &> /dev/null
+		python2 qca-swiss-army-knife/tools/scripts/ath10k/ath10k-bdencoder -c "${JSON}" -o "${PATH_BRD_MFILE}" &> /dev/null
 		if [ $? -eq 0 ]; then
 			echo "Done"
 		else
@@ -231,7 +240,44 @@ if [ ${COPY_ERR} -eq 0 ]; then
 	fi
 	cd "${CWD}"
 
+	# Create ath10k fw directory
+	echo -e "\n${TXT_UNDERLINE}Atheros ath10k firmware${TXT_NORMAL}"
+	PATH_FW_ATH10K="${CWD}/WCN3990/hw1.0"
+	if [ -e "${PATH_FW_ATH10K}" ]; then
+		echo "Deleting existing copy of ath10k firmware files..."
+		rm -rf "${PATH_FW_ATH10K}"
+	fi
+	echo -n "Creating directory for ath10k firmware files: "
+	mkdir -p "${PATH_FW_ATH10K}" &> /dev/null
+	if [ $? -eq 0 ]; then
+		echo "Done"
+
+		echo -n "Copying merged board file: "
+		cp "${PATH_BRD_MAKE}"/"${PATH_BRD_MFILE}" "${PATH_FW_ATH10K}" &> /dev/null
+		if [ $? -eq 0 ]; then
+			echo "Done"
+		else
+			echo "Failed"
+			exit
+		fi
+
+		echo -n "Fetching firmware-5.bin: "
+		wget "${URL_FW_FIRMWARE5BIN}" &> /dev/null
+		if [ $? -eq 0 ]; then
+			echo "Done"
+		else
+			echo "Failed"
+			exit
+		fi
+
+	else
+		echo "Failed"
+		exit
+	fi
+	cd "${CWD}"
+
 	# Create linux dsp directory
+	echo -e "\n${TXT_UNDERLINE}Qualcomm DSP firmware${TXT_NORMAL}"
 	PATH_FW_C630="${CWD}/c630"
 	if [ -e "${PATH_FW_C630}" ]; then
 		echo "Deleting existing copy of linux DSP files..."
