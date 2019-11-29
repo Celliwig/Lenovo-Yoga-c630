f3fb659bd1db4486d141e0003233395d82678667
charles <celliwig@nym.hush.com>
2019-11-29 01:28:24 +0000

    install-helper: initrd build script generates chroot to use as template, then creates initrd archive from it.

--- install-helper/build_initrd.sh
+++ install-helper/build_initrd.sh
@@ -20,11 +20,35 @@ TXT_NORMAL="\033[0m"
 CWD=$PWD
 DIR_INITRD="${CWD}/initrd"
 DIR_MAKEJAIL="${CWD}/makejail"
+DIR_FILES="${CWD}/files"
 
 echo -e "${TXT_UNDERLINE}Creating initrd.img...${TXT_NORMAL}"
+
+# Run sudo to make sure we're authenticated (and not mess with displayed text)
+sudo ls &> /dev/null
 
 echo -n "	Generating makejail configuration: "
 MAKEJAIL_CFG="${DIR_MAKEJAIL}/install-helper-initrd.py"
 MAKEJAIL_CFG_PRE="${DIR_MAKEJAIL}/install-helper-initrd.py.pre"
 sed "s|###PROJECT_DIR###|${DIR_INITRD}|g" "${MAKEJAIL_CFG_PRE}" > "${MAKEJAIL_CFG}"
+okay_failedexit $?
+
+echo -n "	Running makejail: "
+sudo makejail "${MAKEJAIL_CFG}" &> /dev/null
+okay_failedexit $?
+
+if [ ! -d "${DIR_FILES}" ]; then
+	echo -n "	Creating output directory: "
+	mkdir "${DIR_FILES}" &> /dev/null
+	okay_failedexit $?
+fi
+
+echo -n "	Generating cpio image: "
+INITRD_PATH="${DIR_FILES}/initrd"
+cd "${DIR_INITRD}"
+sudo find . | sudo cpio -H newc -o 2> /dev/null 1> "${INITRD_PATH}"
+okay_failedexit $?
+cd "${CWD}"
+echo -n "	Compressing initrd: "
+gzip -f "${INITRD_PATH}" &> /dev/null
 okay_failedexit $?
