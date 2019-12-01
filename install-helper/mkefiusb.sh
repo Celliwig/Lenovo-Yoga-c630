56f1435db6fe0bc614bf62dce9b132b342a8ce27
charles <celliwig@nym.hush.com>
2019-12-01 21:22:44 +0000

    install-helper: 'mkefiusb' updated flags, added sanity checks.

--- install-helper/mkefiusb.sh
+++ install-helper/mkefiusb.sh
@@ -12,8 +12,8 @@ Kernel package (Debian/RedHat supported) is optional.
 If the kernel package is specified it is included on the EFI partition, and in the initrd image.
 
 Options:
-	-d <device name>	- Device to prep.
-	-g <grub package>	- Grub EFI package (optional).
+	-d <device name>	- Target device (mandatory).
+	-g			- Build grub (optional).
 	-k <kernel package>	- Kernel package to include (optional).
 	-m <module name>	- Load named module on startup (optional).
 
@@ -23,11 +23,11 @@ EOF
 }
 
 BLOCK_DEVICE=""
-GRUB_PACKAGE=""
+BUILD_GRUB=false
 KERNEL_PACKAGE=""
 MODULE_LIST=()
 # Pass arguments
-while getopts ":k:m:" opt; do
+while getopts ":d:gk:m:" opt; do
 	case $opt in
 		d)
 			if [[ "${BLOCK_DEVICE}" == "" ]]; then
@@ -37,11 +37,7 @@ while getopts ":k:m:" opt; do
 			fi
 			;;
 		g)
-			if [[ "${GRUB_PACKAGE}" == "" ]]; then
-				GRUB_PACKAGE="${OPTARG}"
-			else
-				print_usage
-			fi
+			BUILD_GRUB=true
 			;;
 		k)
 			if [[ "${KERNEL_PACKAGE}" == "" ]]; then
@@ -58,5 +54,36 @@ while getopts ":k:m:" opt; do
 			;;
 	esac
 done
+# If target not specified, print usage and exit
+if [ -z "${BLOCK_DEVICE}" ]; then print_usage; fi
+
+echo -e "${TXT_UNDERLINE}Running sanity checks: ${TXT_NORMAL}"
+echo -n "	Checking target is block device: "
+BLOCK_DEVNAME=`basename "${BLOCK_DEVICE}"`
+if [ -e /sys/block/"${BLOCK_DEVNAME}" ]; then
+	echo "Okay"
+else
+	echo "Failed"
+	exit 1
+fi
+echo -n "	Check target device is not mounted: "
+mount |grep "${BLOCK_DEVICE}" &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "Okay"
+else
+	echo "Failed"
+	exit 1
+fi
+if [ -n "${KERNEL_PACKAGE}" ]; then
+	echo -n "	Checking kernel package [\"${KERNEL_PACKAGE}\"] type: "
+	KERNEL_PACKAGE_TYPE=`identify_package_type "${KERNEL_PACKAGE}"`
+	if [[ "${KERNEL_PACKAGE_TYPE}" == "unknown" ]]; then
+		echo "Failed"
+		exit 1
+	else
+		echo "${KERNEL_PACKAGE_TYPE}"
+	fi
+fi
+echo
 
 echo -e "${TXT_UNDERLINE}Create an EFI compatible USB key...${TXT_NORMAL}"
