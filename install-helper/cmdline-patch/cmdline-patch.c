9f9a5c00cbfd9c0694b60c8ede4256f4cb6fe7b9
charles <celliwig@nym.hush.com>
2020-01-12 01:46:02 +0000

    cmdline-patch: Implementation using select rather than libev.

--- install-helper/cmdline-patch/cmdline-patch.c
+++ install-helper/cmdline-patch/cmdline-patch.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <libaudit.h>
 #include <unistd.h>
 #include <ev.h>
 #include <errno.h>
 #include "auditctl-llist.h"
 
-//#include "config.h"
-//#include <string.h>
-//#include "private.h"
+int fd;
 
-//void monitoring(struct ev_loop *loop, struct ev_io *io, int revents) {
-//    struct audit_reply reply;
-//
-//    audit_get_reply(fd, &reply, GET_REPLY_NONBLOCKING, 0);
-//
-//    if (reply.type != AUDIT_EOE &&
-//            reply.type != AUDIT_PROCTITLE &&
-//            reply.type != AUDIT_PATH) {
-//        printf("Event: Type=%s Message=%.*s\n",
-//                     audit_msg_type_to_name(reply.type),
-//                     reply.len,
-//                     reply.message);
-//    }
-//}
+void monitoring(struct ev_loop *loop, struct ev_io *io, int revents) {
+	struct audit_reply reply;
+
+	audit_get_reply(fd, &reply, GET_REPLY_NONBLOCKING, 0);
+	if (reply.type != AUDIT_EOE && reply.type != AUDIT_PROCTITLE && reply.type != AUDIT_PATH) {
+//		if (reply.type != NULL) {
+			printf("Event: Type=%s Message=%.*s\n",
+				audit_msg_type_to_name(reply.type),
+				reply.len,
+				reply.message);
+//		}
+	}
+}
 
 /* Returns 0 for success and -1 for failure */
 int delete_all_rules(int fd)
@@ -111,21 +109,84 @@ int main() {
 	audit_rule_fieldpair_data(&rule_new, arch, AUDIT_FILTER_EXIT);
 	char path[] = "path=/projects/Lenovo/Lenovo-Yoga-c630/install-helper/cmdline-patch/tmp_dir";
 	audit_rule_fieldpair_data(&rule_new, path, AUDIT_FILTER_EXIT);
-	char key[] = "key=mount";
+	char key[] = "key=mount_proc";
 	audit_rule_fieldpair_data(&rule_new, key, AUDIT_FILTER_EXIT);
 	audit_add_rule_data(fd, rule_new, AUDIT_FILTER_EXIT, AUDIT_ALWAYS);
 
+	if ((audit_is_enabled(fd) < 2) && (audit_set_enabled(fd, 1) < 0)) {
+		printf("Failed to enable.\n");
+		return 1;
+	}
+	if (audit_set_pid(fd, getpid(), WAIT_YES) < 0) {
+		printf("Failed to set pid.\n");
+		return 1;
+	}
 
-//    struct ev_io monitor;
-//    audit_set_pid(fd, getpid(), WAIT_YES);
+//	struct ev_io monitor;
+//	struct ev_loop *loop = ev_default_loop(EVFLAG_NOENV);
+//
+//	ev_io_init(&monitor, monitoring, fd, EV_READ);
+//	ev_io_start(loop, &monitor);
+//
+//	ev_loop(loop, 0);
+////	ev_run(loop, 0);
+//
+//	ev_io_stop (loop, &monitor);
+//	ev_default_destroy();
 
-//    audit_set_enabled(fd, 1);
-//    struct ev_loop *loop = ev_default_loop(EVFLAG_NOENV);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//    ev_io_init(&monitor, monitoring, fd, EV_READ);
-//    ev_io_start(loop, &monitor);
+	fd_set master;
+	fd_set read_fds;
+	FD_ZERO(&master);
+	FD_ZERO(&read_fds);
+	FD_SET(fd, &master);
 
-//    ev_loop(loop, 0);
+	int i, rc, retval = 0;
+	int timeout = 40; /* tenths of seconds */
+	struct audit_reply reply;
+	const char prockey[] = "key=\"mount_proc\"";
+	while(true){
+		read_fds = master;
+
+		for (i = 0; i < timeout; i++) {
+			struct timeval t;
+
+			t.tv_sec  = 0;
+			t.tv_usec = 100000; /* .1 second */
+			do {
+				rc = select(fd+1, &read_fds, NULL, NULL, &t);
+			} while (rc < 0 && errno == EINTR);
+			// We'll try to read just in case
+			rc = audit_get_reply(fd, &reply, GET_REPLY_NONBLOCKING, 0);
+			if (rc > 0) {
+				/* Reset timeout */
+				i = 0;
+
+				///* Don't make decisions based on wrong packet */
+				//if (reply.nlh->nlmsg_seq != seq)
+				//	continue;
+
+				/* If we get done or error, break out */
+				if (reply.type == NLMSG_DONE)
+					break;
+
+					if (reply.type == NLMSG_ERROR && reply.error->error) {
+					retval = -1;
+					break;
+				}
+
+				if (reply.type == AUDIT_SYSCALL && (strstr(reply.message, prockey) != NULL)) {
+					printf("Event: Type=%s Message=%.*s\n",
+						audit_msg_type_to_name(reply.type),
+						reply.len,
+						reply.message);
+				}
+			}
+		}
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	audit_close(fd);
 	return 0;
